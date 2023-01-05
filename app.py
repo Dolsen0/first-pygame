5eeb1893479d0f531905f0f2b9d19847af982118
dolsen0 <derek.olsen@gmail.com>
2023-01-05 12:23:42 -0500

    adjusted background color from black to sky blue

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import pygame
 
 pygame.init()
 
+background = (0,100,200)
+player = (255,255,255)
+
 canvas = pygame.display.set_mode((1280, 720))
 
 pygame.display.set_caption("Ready?")
 exit = False
 
 while not exit:
+    canvas.fill(background)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
